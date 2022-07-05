6f0dc9127bbe1d439ba265b1a60d94816eed82ca
tusharjha44 <85125680+tusharjha44@users.noreply.github.com>
2022-07-05 19:03:57 +0530

    Initial Commit.

---
+++ app/src/main/java/com/example/socialx/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.socialx
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
+
+
+
